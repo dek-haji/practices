844e6e9be32b5d0dd42e5803fd801ce32c1584c2
dek <deeeqhaji@gmail.com>
2019-03-04 13:37:08 -0600

    new practice

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
-var date = new Date;
-console.log(date)
-var mm = date.getMonth()+ 1;
-var dd = date.getDate()
-var yy = date.getFullYear()
-if (dd < 10){
-    dd='0'+dd
+function print_page() {
+    window.print()
 }
-if (mm <10){
-    mm='0'+mm
-}
-today = mm+'-'+dd+'-'+yy;
-console.log(today)
-console.log(`${mm}-${dd}-${yy}`)
